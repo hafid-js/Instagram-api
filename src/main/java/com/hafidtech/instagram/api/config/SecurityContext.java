649cd3b46f82eba48a2ccad2c84f41620f705600
hafidjs <afidzpratama@gmail.com>
2024-03-19 11:47:04 +0700

    add security with jwt token

---
+++ src/main/java/com/hafidtech/instagram/api/config/SecurityContext.java
@@ -0,0 +1,7 @@
+package com.hafidtech.instagram.api.config;
+
+public class SecurityContext {
+
+    public static final String JWT_KEY="heuhdasdhbashdbsdsbdjkaudsajdb";
+    public static final String HEADER="Authorization";
+}
